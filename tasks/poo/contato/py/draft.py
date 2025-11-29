c43335e9333ecd232e3b0264f5a0923efc4e2c14
deboral6 <deboralemos997130537@gmail.com>
2025-11-29 16:39:20 +0000

    contato

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,109 +1,72 @@
 class Fone:
-    validos = "0123456789()."
-
     def __init__(self, id: str, number: str):
         self.id = id
-        self.number = number
+        self.number = number 
 
-    def getId(self):
-        return self.id
-
-    def getNumber(self):
-        return self.number
-
-    def validar(self, number):
-        for n in number:
-            if n not in self.validos: 
+    def validar(self) -> bool:
+        validos = "0123456789()-."
+        for n in self.number:
+            if n not in validos:
                 return False
         return True
 
-    def __str__(self):
-        return f"{self.id}: {self.number}"
-
+    def __str__(self) -> str:
+        return f"{self.id}:{self.number}"
 
 class Contact:
     def __init__(self, name: str):
         self.name = name
-        self.favorited: bool = False
+        self.favorited = False
         self.fones: list[Fone] = []
 
-    def getFones(self):
-        return self.fones
-
-    def getName(self):
-        return self.name
-
-    def addFone(self, fone: Fone):
-        if fone.validar(fone.number): 
+    def addFone(self, id: str, number: str) -> None:
+        fone = Fone(id, number)
+        if fone.validar():
             self.fones.append(fone)
         else:
-            print("numero invalido")
+            print("fail: invalid number")
 
-    def removeFone(self, valor: int):
-        if 0 <= valor < len(self.fones):
-            self.fones.pop(valor)
+    def removeFone(self, index: int) -> None:
+        if 0 <= index < len(self.fones):
+            self.fones.pop(index)
         else:
-            print("indice inválido")
-    
-    def ifFavorited(self):
-        self.favorited = True
-    
-    def desfavoritar(self):
-        self.favorited = False
+            print("fail: indice invalido")
+
+    def isFavorited(self) -> bool:
+        return self.favorited
+
+    def toggleFav(self) -> None:
+        self.favorited = not self.favorited
 
     def __str__(self):
-        if self.favorited:
-            prefixo = "@"
-        else:
-            prefixo = "-"
-        fones_str = ""
-        for i in range(len(self.fones)):
-            fone = self.fones[i]
-            fones_str += f"{i}:{fone.id}:{fone.number}"
-            if i < len(self.fones) - 1:
-                fones_str += ", "
-        return f"{prefixo} {self.name} [{fones_str}]"
+        fav = "@" if self.favorited else "-"
+        fones_str = ", ".join(str(f) for f in self.fones)
+        return f"{fav} {self.name} [{fones_str}]"
 
 def main():
-    contact = None
+    contact: Contact | None = None
 
     while True:
-        try:
-            line = input()
-        except EOFError:
-            break
-
-        if not line:
-            continue
-
+        line = input()
+        print(f"${line}")
         args = line.split()
 
-        # Comando init, add, rm, tfav, end → ecoa $ antes
-        if args[0] in ["init", "add", "rm", "tfav", "end"]:
-            print(f"${line}")
+        if args[0] == "end":
+            break
 
-        if args[0] == "init":
+        elif args[0] == "init":
             contact = Contact(args[1])
-            print(contact)
 
         elif args[0] == "show":
-            print(contact)  # NÃO imprime $show
+            print(contact)
 
         elif args[0] == "add":
-            contact.addFone(Fone(args[1], args[2]))
-            print(contact)
+            contact.addFone(args[1], args[2])
 
         elif args[0] == "rm":
             contact.removeFone(int(args[1]))
-            print(contact)
 
         elif args[0] == "tfav":
-            if contact.favorited:
-                contact.desfavoritar()
-            else:
-                contact.ifFavorited()
-            print(contact)
+           contact.toggleFav()
 
-        elif args[0] == "end":
-            print(f"${line}")
-            break
+main()
